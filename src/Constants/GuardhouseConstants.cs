37581ecb32830b966cc5636aecd515a936bb19d1
Leonid <tops@topkek.kek>
2026-02-20 00:03:04 +0100

    Update GuardhouseConstants.cs

--- src/Constants/GuardhouseConstants.cs
+++ src/Constants/GuardhouseConstants.cs
@@ -61,6 +61,7 @@ public static class GuardhouseConstants
         public const double ClockSkewMinutes = 5.0;
         public const string DefaultScope = "api";
         public const int IntrospectionCacheTtlSeconds = 5;
+        public const int RefreshTokenCacheDurationDays = 30;
     }
 
     public static class Validation
